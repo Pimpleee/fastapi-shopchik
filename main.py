976274cf264a8c4f39ce4bfd50b8bf41c88e829c
Pimple <07700660gg@mail.ru>
2025-05-22 15:01:53 +0300

    new black formatter + full crud

--- main.py
+++ main.py
@@ -4,56 +4,24 @@ import uvicorn
 from fastapi import FastAPI, Path
 from pydantic import EmailStr, BaseModel
 
+from items_views import router as items_router
+from users.views import router as users_router
+
 app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
 
 
-class CreateUser(BaseModel):
-    email: EmailStr
-
-
-@app.get('/')
+@app.get("/")
 async def hello_index():
-    return {
-        'message': 'Hello Index!!'
-    }
+    return {"message": "Hello Index!!"}
 
 
-@app.get('/hello')
-async def hello(name: str = 'World'):
+@app.get("/hello")
+async def hello(name: str = "World"):
     name = name.strip().title()
-    return {'message': f'Hello {name}'}
+    return {"message": f"Hello {name}"}
 
 
-@app.post('/users')
-async def create_user(user: CreateUser):
-    return {
-        'message': 'success',
-        'email': user.email
-    }
-
-
-@app.get('/items')
-async def list_items():
-    return [
-        'item1',
-        'item2',
-        'item3',
-    ]
-
-
-@app.get('/items/{item_id}')
-async def get_item_by_id(item_id: Annotated[int, Path(ge=1, lt=1000000)]):
-    return {
-        'items': {
-            'id': item_id,
-        }
-    }
-
-
-@app.get('/items/latest')
-async def get_latest_item():
-    return {'item': {'id': '0', 'name': 'latest'}}
-
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
