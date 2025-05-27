cf1b9969a682f5966f0cc423556dd430c4d6fe25
Pimple <07700660gg@mail.ru>
2025-05-28 02:59:58 +0300

    ready and create,good night

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+from contextlib import asynccontextmanager
 from typing import Annotated
 
 import uvicorn
 from fastapi import FastAPI, Path
 from pydantic import EmailStr, BaseModel
-
+from core.models import Base, db_helper
 from items_views import router as items_router
 from users.views import router as users_router
+from api import router as router_v1
+from core.config import settings
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_prefix)
 app.include_router(items_router)
 app.include_router(users_router)
 
